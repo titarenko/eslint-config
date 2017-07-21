8639146c43b82bb8ba7fa77c5a3a37eff3260922
Constantin Titarenko <constantin.titarenko@gmail.com>
2017-07-21 22:38:07 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,5 @@
+module.exports = {
+  plugins: ['require-path-exists'],
+  extends: ['standard', 'plugin:require-path-exists/recommended'],
+  rules: { 'comma-dangle': ['error', 'always-multiline'] }
+}
