66876d58e8ac01c466cda127549273e1ed10567a
Constantin Titarenko <constantin.titarenko@gmail.com>
2017-08-03 16:10:14 +0300

    disable no-mixed-operators

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 module.exports = {
   plugins: ['require-path-exists'],
   extends: ['standard', 'plugin:require-path-exists/recommended'],
-  rules: { 'comma-dangle': ['error', 'always-multiline'] }
+  rules: {
+    'comma-dangle': ['error', 'always-multiline'],
+    'no-mixed-operators': ['off']
+  }
 }
